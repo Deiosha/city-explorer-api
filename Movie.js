3f69ae570dc58260e0dd0dc568ee7794512282a6
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-12-09 18:31:41 -0800

    added cache hit, miss, and timestap

--- Movie.js
+++ Movie.js
@@ -1,17 +1,27 @@
 const  axios = require("axios");
 
+const cache = {};
+
 async function getMovie(request, response) {
   console.log('movie function');
+  
   try {
     let movieKey = process.env.MOVIE_API_KEY;
     let movieQuery = request.query.searchQuery;
+    let key = 'movie' + movieQuery;
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${movieQuery}`;
     console.log(url);
-    let cityMovie = await axios.get(url);
-    console.log(cityMovie.data.results);
-    let movieArray = cityMovie.data.results.map(movie => new Movie(movie));
-    // console.log(movieArray);
-    response.send(movieArray);
+    if (cache[key]&& (Date.now() - cache[key].timestamp < 50000)){
+      console.log('cache hit');
+    } else {
+      console.log('cache miss');
+      cache[key]={}
+      let cityMovie = await axios.get(url);
+      console.log(cityMovie.data.results);
+      cache[key].data = cityMovie.data.results.map(movie => new Movie(movie));
+    }
+    console.log(cache[key].data);
+    response.send(cache[key].data);
   } catch (error) {
     response.send(error.message);
   }
--- weather.js
+++ weather.js
@@ -1,21 +1,30 @@
 const  axios = require("axios");
 
+const cache = {};
+
+
 const getWeather = async (request, response, next) => {
   const { searchQuery } = request.query;
-  if (!request.query.searchQuery) {
-    next('Please enter location');
+  if (cache[searchQuery]) {
+    console.log('hit');
   } else {
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=6&city=${searchQuery}`
-    let cityForecast = await axios.get(url)
-    // console.log(foundCity);
-    let dayArray = cityForecast.data.data;
-    let forecastArray = dayArray.map(day => new Forecast(day));
-    console.log(forecastArray);
-
-   
-    response.send(forecastArray);
-
+    if (!request.query.searchQuery) {
+      next('Please enter location');
+    } else {
+      let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=6&city=${searchQuery}`
+      let cityForecast = await axios.get(url)
+      // console.log(cityForecast);
+      let dayArray = cityForecast.data.data;
+      let forecastArray = dayArray.map(day => new Forecast(day));
+      console.log(forecastArray);
+      console.log('miss');
+  
+     
+      cache[searchQuery] = forecastArray;
+  
+    }
   }
+  response.send(cache[searchQuery]);
 }
 
 class Forecast {
